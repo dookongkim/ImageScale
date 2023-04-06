7953dfc4745666b6c8cfb4f21397279759a8844d
gkswns103 <gkswns103@naver.com>
2023-04-06 10:31:53 +0900

    Initial commit

---
+++ app/src/androidTest/java/MyImageView.java
@@ -0,0 +1,37 @@
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+
+//commit1 - 기본 설계 완료
+
+
+public class MyImageView extends View {
+    Drawable image;
+    float scale = 1f;
+
+
+    public MyImageView(Context context) {
+        super(context);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+
+    class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        public boolean onScale(ScaleGestureDetector detector) {
+
+            return true;
+    }
+    }
+
+}
